0407f098a70889195cb88ff9a42b2b2d08ee2ec3
Victor Guedes <victorguedes68@hotmail.com>
2020-03-25 17:50:55 -0300

    Sessions

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,13 @@ const express = require('express')
 const OngController = require('./controllers/OngController')
 const IncidentCrontroller = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
 // const connection = require('./database/connection')
 
 const routes = express.Router()
+
+routes.get('/', (req,res)=>{return res.json(res.body)})
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
@@ -13,6 +16,10 @@ routes.post('/ongs', OngController.create)
 routes.get('/incidents', IncidentCrontroller.index)
 routes.post('/incidents', IncidentCrontroller.create)
 routes.delete('/incidents/:id', IncidentCrontroller.delete)
-routes.get('/incidents', ProfileController.index)
+
+routes.get('/profile', ProfileController.index)
+
+routes.post('/session', SessionController.create)
+// routes.post('/session/:id', SessionController.delete)
 
 module.exports = routes
