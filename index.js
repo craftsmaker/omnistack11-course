f80bcc9ca011bb95826f944650060bc000c7e858
Victor Guedes <victorguedes68@hotmail.com>
2020-03-25 11:46:50 -0300

    Packages

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const express = require('express')
 
 const app = express()
 
-app.post('/users', (request,response)=>{
+app.use(express.json())
+app.get('/user', (request,response)=>{
+	console.log(request.body)
 	return response.json({
 		name:'tiago',
 	})
 })
 
 app.listen(3333)
+
+
+/*
+	Express:
+
+	Route: localhost
+	Resource:/user
+*/
+/*
+	Métodos HTTP:
+
+	GET: Buscar/listar informações ""
+	POST: Criar informação ""
+	PUT: Alterar informação no ""
+	DELETE: Deletar informação "" 
+*/
+
+/*
+	Tipos de parâmetros:
+
+	Query params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+	Route params: Parâmetros utilizados para identificar recursos
+	Request body
+*/
