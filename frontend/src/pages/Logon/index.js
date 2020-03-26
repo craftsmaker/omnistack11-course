86eae323962fdd1639b3bc20b22afba6864ae7c8
Victor Guedes <victorguedes68@hotmail.com>
2020-03-26 17:58:11 -0300

    Register stylized

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './style.css'
 
@@ -17,10 +18,10 @@ export default class Logon extends React.Component {
 						<input placeholder="Sua ID"/>
 						<button className="button" type="submit">Entrar</button>
 
-						<a href="/register">
+						<Link to="/register" className="back-link">
 							<FiLogIn size={16} color="#E02041"/>
 							Não tenho cadastro
-						</a>
+						</Link>
 					</form>
 				</section>
 
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import './style.css'
+import {FiArrowLeft} from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg'
+
+export default function Register(){
+	return(
+		<div className="register-container">
+			<div className="content">
+				<section>
+					<img src={logoImg} alt="Be The Hero"/>
+
+					<h1>Cadastro</h1>
+					<p>Faça seu cadastro, entre na plataforma e ajude as pessoas a encontrarem os casos da sua ONG.</p>
+					<Link to="/" className="back-link">
+						<FiArrowLeft/>
+						Tenho conta
+					</Link>
+				</section>
+
+				<form>
+					<input placeholder="Nome da ONG"/>
+					<input type="email" placeholder="E-mail"/>
+					<input type="email" placeholder="Whatsapp"/>
+
+					<div className="input-group">
+						<input placeholder="Cidade"/>
+						<input placeholder="UF" style={{width:80}}/>
+					</div>
+
+					<button className="button" type="submit">Cadastrar</button>
+				</form>
+			</div>
+		</div>
+	)
+}
